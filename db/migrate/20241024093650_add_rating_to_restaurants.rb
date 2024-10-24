72d8bb456bdf894cc8efacc39d045894535d1779
Douglas Berkley <douglasmberkley@gmail.com>
2024-10-24 11:21:12 +0900

    added def change

--- db/migrate/20241024093650_add_rating_to_restaurants.rb
+++ db/migrate/20241024093650_add_rating_to_restaurants.rb
@@ -1,4 +1,6 @@
 class AddInfoToRestaurants < ActiveRecord::Migration[7.0]
   # add_column :table_name, :column_name, :data_type, options
-  add_column :restaurants, :rating, :integer, default: 0, null: false
+  def change
+    add_column :restaurants, :rating, :integer, default: 0, null: false
+  end
 end
