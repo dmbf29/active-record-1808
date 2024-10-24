9ca5e66da68bec652d00fccf001a9d3e6b3eba3d
Douglas Berkley <douglasmberkley@gmail.com>
2024-10-24 11:05:08 +0900

    added def change

--- db/migrate/20241024092408_create_restaurants.rb
+++ db/migrate/20241024092408_create_restaurants.rb
@@ -6,10 +6,12 @@
 # Adding a column:
 # timestamp_add_column_to_table.rb
 class CreateRestaurants < ActiveRecord::Migration[7.0]
-  create_table :restaurants do |t|
-    t.string :name
-    t.string :address
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
 
-    t.timestamps # adds two columns (created_at / updated_at)
+      t.timestamps # adds two columns (created_at / updated_at)
+    end
   end
 end
